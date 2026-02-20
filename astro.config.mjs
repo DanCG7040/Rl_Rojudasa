65b1b493f66e0c327963b5453d5a964b56d9e2bd
DanCG7040 <smittenspider8@hotmail.com>
2026-02-20 14:59:28 -0500

    repared

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,8 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import vercel from '@astrojs/vercel/serverless';
 
-// https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: vercel(),
   integrations: [react()],
-  output: 'hybrid', // Páginas estáticas + API routes como serverless en Vercel
-  adapter: vercel({
-    maxDuration: 10,
-  }),
-  build: {
-    assets: 'assets'
-  }
 });
